65213363b7e4e760d37401aa246b405f641a4fc5
supermoneylife <davejn82@gmail.com>
2020-08-21 11:52:01 +0100

    Desk Collision added. Many files removed

--- on_the_run_zeus/interrupt.asm
+++
@@ -1,9 +0,0 @@
-                ORG     $AD00
-
-IntPage         EQU     HIGH    $
-
-                DEFS    $0101,IntPage+1
-
-                ORG     (IntPage+1)*$0100+(IntPage+1)
-                JP      IntRt
-
